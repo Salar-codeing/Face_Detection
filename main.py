4182359c80a8cf1f25ac8e238796b2b801554711
Salar Hashemian <84159945+Salar-codeing@users.noreply.github.com>
2021-07-22 01:52:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -36,5 +36,5 @@ while True:
     pTime = cTime
     cv2.putText(img, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_PLAIN,
                 3, (0, 255, 0), 2)
-    cv2.imshow("Image", img)
+    cv2.imshow("Face Detection", img)
     cv2.waitKey(1)
